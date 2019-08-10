0605fc0288ea6505446ca1aba3b27f2fd25561f6
patrickcberry <patrickcberry@gmail.com>
2019-08-10 21:36:47 +1000

    General update

--- fetch.sh
+++ fetch.sh
@@ -9,6 +9,6 @@ fi
 
 mkdir /tmp/ubs
 
-wget -O -N /tmp/ubs/update-all.sh https://raw.githubusercontent.com/patrickcberry/ubuntu-scripts/master/update-all.sh
+wget -O /tmp/ubs/update-all.sh https://raw.githubusercontent.com/patrickcberry/ubuntu-scripts/master/update-all.sh
 
 ls /tmp/ubs
