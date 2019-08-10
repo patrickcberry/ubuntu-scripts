b4da389d30c6c08a84aaa9faab288531a338808c
patrickcberry <patrickcberry@gmail.com>
2019-08-10 22:52:16 +1000

    General update

--- fetch.sh
+++ fetch.sh
@@ -10,7 +10,7 @@ fi
 mkdir /tmp/ubs
 
 wget -O /tmp/ubs/update-all.sh https://raw.githubusercontent.com/patrickcberry/ubuntu-scripts/master/update-all.sh
-wget -O /tmp/ubs/default.sh https://raw.githubusercontent.com/patrickcberry/ubuntu-scripts/master/default.sh
+wget --no-cache -O /tmp/ubs/default.sh https://raw.githubusercontent.com/patrickcberry/ubuntu-scripts/master/default.sh
 wget -O /tmp/ubs/netplan.yaml https://raw.githubusercontent.com/patrickcberry/ubuntu-scripts/master/netplan.yaml
 
 ls /tmp/ubs
