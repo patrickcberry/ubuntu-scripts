33b6af54e1ca61c3244c843fce42512cb34160f0
patrickcberry <patrickcberry@gmail.com>
2019-08-10 23:09:56 +1000

    General update

--- fetch.sh
+++ fetch.sh
@@ -12,5 +12,6 @@ mkdir /tmp/ubs
 wget -O /tmp/ubs/update-all.sh https://raw.githubusercontent.com/patrickcberry/ubuntu-scripts/master/update-all.sh
 wget --no-cache -O /tmp/ubs/default.sh https://raw.githubusercontent.com/patrickcberry/ubuntu-scripts/master/default.sh
 wget -O /tmp/ubs/netplan.yaml https://raw.githubusercontent.com/patrickcberry/ubuntu-scripts/master/netplan.yaml
+wget --no-cache -O /tmp/ubs/motion-eye.sh https://raw.githubusercontent.com/patrickcberry/ubuntu-scripts/master/motion-eye.sh
 
 ls /tmp/ubs
