036f37323fff6b5a0d4a0941d3d9ca3761b1d2e4
Patrick Berry <patrickcberry@gmail.com>
2019-08-11 16:17:56 +1000

    Delete update.bat

--- update.bat
+++
@@ -1,9 +0,0 @@
-if "%~1" equ "" (
-	set message="General update"
-) ELSE (
-	set message=%1
-)
-
-git add .
-git commit -m %message%
-git push https://patrickcberry:Asunmaa2!@github.com/patrickcberry/ubuntu-scripts
